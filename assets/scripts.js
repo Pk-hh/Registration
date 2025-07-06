cb4d43778f103997469aa5668f6bd18cb1318c75
Pk-hh <pradeep4045t7@gmail.com>
2025-07-06 19:58:36 +0530

    Add files via upload

---
+++ assets/scripts.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('registrationForm');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const data = {
+    roll: form.roll.value,
+    email: form.email.value,
+    phone: form.phone.value,
+    college: form.college.value
+  };
+
+  fetch("https://script.google.com/macros/s/AKfycbzg1C2y1GEK_cVGHjHA3THjv_ki1n03MQHAdFCe3Fa1WADbLFHnZqsoupYlzF9UADrjDA/exec", {
+    method: "POST",
+    body: JSON.stringify(data),
+  })
+  .then(response => response.text())
+  .then(result => {
+    alert("Registration successful!");
+    form.reset();
+  })
+  .catch(error => {
+    console.error("Error!", error.message);
+    alert("Submission failed.");
+  });
+});
