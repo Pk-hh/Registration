7b3f054071e1779cc4ebbf41cc4e1fca9db19600
Pk-hh <pradeep4045t7@gmail.com>
2025-07-06 21:02:19 +0530

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,27 +1,17 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyfuFiDRU2K2k3h3wUEmS9U4Dh5UKHCkm2TPzR9dv_-c-uJPtw3reviwm4rR_IgneHBeg/exec';
 const form = document.getElementById('registrationForm');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
+  const data = new FormData(form);
 
-  const data = {
-    roll: form.Name.value,
-    roll: form.roll.value,
-    email: form.email.value,
-    phone: form.phone.value,
-    college: form.college.value
-  };
-
-  fetch("https://script.google.com/macros/s/AKfycbyfuFiDRU2K2k3h3wUEmS9U4Dh5UKHCkm2TPzR9dv_-c-uJPtw3reviwm4rR_IgneHBeg/exec", {
-    method: "POST",
-    body: JSON.stringify(data),
-  })
-  .then(response => response.text())
-  .then(result => {
-    alert("Registration successful!");
-    form.reset();
-  })
-  .catch(error => {
-    console.error("Error!", error.message);
-    alert("Submission failed.");
-  });
+  fetch(scriptURL, { method: 'POST', body: data })
+    .then(response => {
+      alert("✅ Successfully Registered!");
+      form.reset();
+    })
+    .catch(error => {
+      alert("❌ Something went wrong.");
+      console.error('Error!', error.message);
+    });
 });
